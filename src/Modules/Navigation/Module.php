1e94a2469958b200d9f89d4d051edcd26a3c2172
Jakub Josef Forman <me@jakubforman.eu>
2022-08-16 02:46:43 +0200

    Fix ACF typo error

--- src/Modules/Navigation/Module.php
+++ src/Modules/Navigation/Module.php
@@ -4,36 +4,35 @@ namespace AwwwesomeEEP\Modules\Navigation;
 
 use AwwwesomeEEP\Modules\Module_Base;
 
-class Module extends Module_Base {
+class Module extends Module_Base
+{
 
 
-	public function __construct() {
-		parent::__construct();
-	}
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	/**
-	 * Widgets
-	 *
-	 * @return string[]
-	 * @throws \Exception
-	 */
-	public function get_widgets() {
-		// Complete list of fidgets
-		if ( function_exists( "get_fields" ) ) {
-			return [
-				'Child_Navigation',
-			];
-		}
+    /**
+     * Widgets
+     *
+     * @return string[]
+     * @throws \Exception
+     */
+    public function get_widgets()
+    {
+        return [
+            'Child_Navigation',
+        ];
+    }
 
-		return [];
-	}
-
-	/**
-	 * Module name
-	 *
-	 * @return string
-	 */
-	public function get_name() {
-		return 'Navigatiion';
-	}
+    /**
+     * Module name
+     *
+     * @return string
+     */
+    public function get_name()
+    {
+        return 'Navigatiion';
+    }
 }
