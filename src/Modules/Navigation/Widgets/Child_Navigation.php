03d7374a3cfe6ade25b99376c1241983b801b423
Jakub Josef Forman <me@jakubforman.eu>
2022-09-13 20:23:31 +0200

    Adding custom trait for any widget border style

--- src/Modules/Navigation/Widgets/Child_Navigation.php
+++ src/Modules/Navigation/Widgets/Child_Navigation.php
@@ -2,6 +2,7 @@
 
 namespace AwwwesomeEEP\Modules\Navigation\Widgets;
 
+use AwwwesomeEEP\Widget_Border;
 use Elementor\Controls_Manager;
 use Elementor\Core\Kits\Documents\Tabs\Global_Colors;
 use Elementor\Group_Control_Border;
@@ -11,6 +12,8 @@ use Elementor\Plugin;
 use Elementor\Widget_Base;
 
 class Child_Navigation extends Widget_Base {
+
+	use Widget_Border;
 
 	public function __construct( $data = [], $args = null ) {
 		parent::__construct( $data, $args );
@@ -123,11 +126,11 @@ class Child_Navigation extends Widget_Base {
 		$this->add_control(
 			'child_navigation_source',
 			[
-				'label'   => esc_html__( 'Show sub-pages of', 'aw3sm-eep' ),
-				'separator'    => 'before',
-				'type'    => Controls_Manager::SELECT,
-				'default' => 'all',
-				'options' => $types,
+				'label'     => esc_html__( 'Show sub-pages of', 'aw3sm-eep' ),
+				'separator' => 'before',
+				'type'      => Controls_Manager::SELECT,
+				'default'   => 'all',
+				'options'   => $types,
 			],
 		);
 		$this->add_control(
@@ -164,7 +167,21 @@ class Child_Navigation extends Widget_Base {
 		$this->add_box_style_controls();
 
 		// border style
-		$this->add_border_style_controls();
+		$this->add_border_style_controls( [
+			'border'                  => '{{WRAPPER}} .child-navigation-container',
+			'box_shadow'              => '{{WRAPPER}} .child-navigation-container',
+			'border_hover'              => '{{WRAPPER}} .child-navigation-container:hover',
+			'box_shadow_hover'              => '{{WRAPPER}} .child-navigation-container:hover',
+			'border_radius'           => [
+				'{{WRAPPER}} .child-navigation-container' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+			],
+			'border_radius_hover'     => [
+				'{{WRAPPER}} .child-navigation-container:hover' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+			],
+			'border_hover_transition' => [
+				'{{WRAPPER}} .child-navigation-container' => 'transition: border {{SIZE}}s, border-radius {{SIZE}}s, box-shadow {{SIZE}}s',
+			],
+		] );
 
 		// register title style section
 		$this->add_title_style_control();
@@ -204,133 +221,6 @@ class Child_Navigation extends Widget_Base {
 	}
 
 
-	function add_border_style_controls(){
-		$this->start_controls_section(
-			'border',
-			[
-				'label' => esc_html__( 'Border', 'aw3sm-eep' ),
-				'tab' => Controls_Manager::TAB_STYLE,
-			]
-		);
-
-		$this->start_controls_tabs( 'tabs_border' );
-
-		$this->start_controls_tab(
-			'tab_border_normal',
-			[
-				'label' => esc_html__( 'Normal', 'aw3sm-eep' ),
-			]
-		);
-
-		$this->add_group_control(
-			Group_Control_Border::get_type(),
-			[
-				'name' => 'border',
-				'selector' => '{{WRAPPER}} .child-navigation-container',
-			]
-		);
-
-
-		$this->add_responsive_control(
-			'border_radius',
-			[
-				'label' => esc_html__( 'Border Radius', 'aw3sm-eep' ),
-				'type' => Controls_Manager::DIMENSIONS,
-				'size_units' => [ 'px', '%' ],
-				'selectors' => [
-					'{{WRAPPER}} .child-navigation-container' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-				],
-			]
-		);
-
-		$this->add_group_control(
-			Group_Control_Box_Shadow::get_type(),
-			[
-				'name' => 'box_shadow',
-				'selector' => '{{WRAPPER}} .child-navigation-container',
-			]
-		);
-
-		$this->end_controls_tab();
-
-		$this->start_controls_tab(
-			'tab_border_hover',
-			[
-				'label' => esc_html__( 'Hover', 'aw3sm-eep' ),
-			]
-		);
-
-		$this->add_group_control(
-			Group_Control_Border::get_type(),
-			[
-				'name' => 'border_hover',
-				'selector' => '{{WRAPPER}} .child-navigation-container:hover',
-			]
-		);
-
-
-		$this->add_responsive_control(
-			'border_radius_hover',
-			[
-				'label' => esc_html__( 'Border Radius', 'aw3sm-eep' ),
-				'type' => Controls_Manager::DIMENSIONS,
-				'size_units' => [ 'px', '%' ],
-				'selectors' => [
-					'{{WRAPPER}} .child-navigation-container:hover' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-				],
-			]
-		);
-
-		$this->add_group_control(
-			Group_Control_Box_Shadow::get_type(),
-			[
-				'name' => 'box_shadow_hover',
-				'selector' => '{{WRAPPER}} .child-navigation-container:hover',
-			]
-		);
-
-		$this->add_control(
-			'border_hover_transition',
-			[
-				'label' => esc_html__( 'Transition Duration', 'aw3sm-eep' ),
-				'type' => Controls_Manager::SLIDER,
-				'separator' => 'before',
-				'default' => [
-					'size' => 0.3,
-				],
-				'range' => [
-					'px' => [
-						'max' => 3,
-						'step' => 0.1,
-					],
-				],
-				/*'conditions' => [
-					'relation' => 'or',
-					'terms' => [
-						[
-							'name' => 'background_background',
-							'operator' => '!==',
-							'value' => '',
-						],
-						[
-							'name' => 'border_border',
-							'operator' => '!==',
-							'value' => '',
-						],
-					]
-				],*/
-				'selectors' => [
-					'{{WRAPPER}} .child-navigation-container' => 'transition: border {{SIZE}}s, border-radius {{SIZE}}s, box-shadow {{SIZE}}s',
-				],
-			]
-		);
-
-		$this->end_controls_tab();
-
-		$this->end_controls_tabs();
-
-		$this->end_controls_section();
-	}
 	/**
 	 * Register style tab
 	 * @return void
@@ -816,7 +706,7 @@ class Child_Navigation extends Widget_Base {
 	function render_nav( int $depth = 0, int $top_page = null, array $exclude = [] ) {
 		$title            = $this->get_settings_for_display( 'title' );
 		$title_responsive = $this->get_settings_for_display( 'toggle_title' );
-		$_finalContent = "<nav class='child-navigation-container'>";
+		$_finalContent    = "<nav class='child-navigation-container'>";
 		// TODO: outside nav
 		$_finalContent .= "<div class='menu-toggle'>";
 		$_finalContent .= "<div class='title'>$title_responsive</div>";
@@ -1030,7 +920,7 @@ class Child_Navigation extends Widget_Base {
 				'type'            => Controls_Manager::RAW_HTML,
 				'raw'             => __( 'For preview change view to responsive mode.',
 					'aw3sm-eep' ),
-				'condition'    => [
+				'condition'       => [
 					'dropdown_breakpoint!' => 'none',
 				],
 				'content_classes' => 'elementor-descriptor',
