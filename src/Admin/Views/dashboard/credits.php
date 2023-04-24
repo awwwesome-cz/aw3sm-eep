7d10336ea7f366ad4ee28f4b9e28af348b1b003e
Jakub Josef Forman <me@jakubforman.eu>
2023-04-24 18:38:20 +0200

    New credits texts

--- src/Admin/Views/dashboard/credits.php
+++ src/Admin/Views/dashboard/credits.php
@@ -1,28 +1,76 @@
 <div id="poststuff">
-    <div id="post-body" class="metabox-holder columns-2">
-        <div id="postbox-container-2" class="postbox-container">
-            <div class="postbox">
-                <div class="postbox-header">
-                    <h2>Plugin Purpose</h2>
-                </div>
-                <div class="inside">
-                    <p>Plugin se zaměřuje na rozšíření pluginů Elementor & Elementor Pro...</p>
-                </div>
-            </div>
-        </div>
-        <div id="postbox-container-1" class="postbox-container">
-            <div class="postbox">
-                <div class="postbox-header">
-                    <h2>Credits</h2>
-                </div>
-                <div class="inside">
-                    <ul>
-                        <li><a href="https://awwwesome.cz" target="_blank">awwwesome.cz</a></li>
-                        <li><a href="https://jakubforman.eu" target="_blank">Jakub Josef Forman</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-    <br class="clear">
+	<div id="post-body" class="metabox-holder columns-2">
+		<div id="postbox-container-2" class="postbox-container">
+			<div class="postbox">
+				<div class="postbox-header">
+					<h2>Důvod pluginu</h2>
+				</div>
+				<div class="inside">
+					<p>
+						Plugin EEP se zaměřuje na rozšíření pluginů Elementor & Elementor Pro. Využívá stejné syntaxe a
+						struktury kódu aby se co nejvíce připodobnil originálním pluginům. Každý widget je ručně
+						vytvořen, aby přesně splňoval veškeré vlastnosti Elementor widgetů. Každý plugin využívá
+						dědičnost tak aby při aktualizaci Elementoru bylo minimalizováno riziko nefunkčnosti pluginu
+						nebo některé z jeho částí.
+					</p>
+				</div>
+			</div>
+			<div class="postbox">
+				<div class="postbox-header">
+					<h2>Podpora a údržba</h2>
+				</div>
+				<div class="inside">
+					<p>
+						Plugin je primárně vyvíjen a udržován za účelem nasanení na webech vytvořených a spravovaných
+						agenturou <a href="https://awwwesome.cz" target="_blank">awwwesome.cz</a> nebo
+						<a href="https://jakubforman.eu" target="_blank">Jakubem Josefem Formanem</a>. Aktualizace
+						pluginy vycházejí tehdy, pokud je to nutné (bezpečností záplaty) nebo některý z klientů vyžaduje
+						rozšíření.
+					</p>
+					<p>
+						Pro tento plugin existuje i placené rozšíření <strong>Elementor Extension Pack Pro</strong>,
+						který má zvýšenou podporu a klienti
+						<a href="https://awwwesome.cz" target="_blank">awwwesome.cz</a> nebo
+						<a href="https://jakubforman.eu" target="_blank">jakubfoman.eu</a> mají tento modul zdarma.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div id="postbox-container-1" class="postbox-container">
+			<div class="postbox">
+				<div class="postbox-header">
+					<h2>Autor & Licence</h2>
+				</div>
+				<div class="inside">
+					<p>Tento plugin byl napsán a vytvořen <a href="https://jakubforman.eu">Jakubem Josefem Formanem</a>.
+						A je licencován jako Free Software pod licencí ...
+						<!-- TODO: It is licensed as Free Software under GNU General Public License 2 (GPL 2).-->
+					</p>
+				</div>
+			</div>
+			<div class="postbox">
+				<div class="postbox-header">
+					<h2>Credits</h2>
+				</div>
+				<div class="inside">
+					Hlavní dík patří:
+					<ul>
+						<li><a href="https://awwwesome.cz" target="_blank">awwwesome.cz</a></li>
+						<li><a href="https://jakubforman.eu" target="_blank">Jakub Josef Forman</a></li>
+						<li>všem zákazníkům, podporovatelům, přispěvatelům a uživatelům tohoto pluginu.</li>
+					</ul>
+				</div>
+			</div>
+			<div class="postbox">
+				<div class="postbox-header">
+					<h2>Přispívání</h2>
+				</div>
+				<div class="inside">
+					<p>Pro zájemce, kterí by rádi plugin dále rozvýjeli je zde možnost skrze oficiální
+						<a href="https://github.com/awwwesome-cz/aw3sm-eep" target="_blank">GitHub Repizitář</a>.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br class="clear">
 </div>
