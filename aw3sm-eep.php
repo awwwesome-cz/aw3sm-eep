9836986107a89c014cdebaf150303d727fd9d522
Jakub Josef Forman <me@jakubforman.eu>
2023-04-24 11:13:14 +0200

    Releasing new FIX version

--- aw3sm-eep.php
+++ aw3sm-eep.php
@@ -2,8 +2,8 @@
 /**
  * Plugin Name: AW3SM Elementor Extension Pack
  * Description: Elementor Extension pack is an extension plugin for Elementor page builder. Adding many widgets.
- * Version: 1.5.0
- * Stable tag: 1.5.0
+ * Version: 1.5.1
+ * Stable tag: 1.5.1
  * Requires PHP: 7.4
  * Tested up to: 6.2
  * Requires at least: 6.0
