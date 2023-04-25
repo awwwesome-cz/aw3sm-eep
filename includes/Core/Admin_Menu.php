32888daa4257b8354fd6ba7816735dd7fe9a14a8
Jakub Josef Forman <me@jakubforman.eu>
2023-04-25 11:55:19 +0200

    Adding sub menu

--- includes/Core/Admin_Menu.php
+++ includes/Core/Admin_Menu.php
@@ -11,6 +11,7 @@ class Admin_Menu {
 	 * @var array
 	 */
 	protected static array $pages = [];
+	protected static array $sub_pages = [];
 	private static ?string $slug_prefix = null;
 
 	/**
@@ -20,9 +21,10 @@ class Admin_Menu {
 	 * @param $menu_title
 	 * @param $slug
 	 * @param $capability
-	 * @param $callback
-	 * @param $icon_url
-	 * @param $position
+	 * @param string $callback
+	 * @param string $icon_url
+	 * @param null $position
+	 * @param bool $remove_from_submenu
 	 *
 	 * @return void
 	 */
@@ -31,33 +33,97 @@ class Admin_Menu {
 		$menu_title,
 		$slug,
 		$capability,
-		$callback = '',
-		$icon_url = '',
-		$position = null
+		string $callback = '',
+		string $icon_url = '',
+		$position = null,
+		bool $remove_from_submenu = true
 	) {
 		self::$pages[ $slug ] = [
-			"slug"       => $slug,
-			"capability" => $capability,
-			"menu_title" => $menu_title,
-			"page_title" => $page_title,
-			"callback"   => $callback,
-			"icon_url"   => $icon_url,
-			"position"   => $position,
+			"slug"                => $slug,
+			"capability"          => $capability,
+			"menu_title"          => $menu_title,
+			"page_title"          => $page_title,
+			"callback"            => $callback,
+			"icon_url"            => $icon_url,
+			"position"            => $position,
+			"remove_from_submenu" => $remove_from_submenu
+		];
+	}
+
+	/**
+	 * Add new sub-page to admin
+	 *
+	 * @param $page_title
+	 * @param $menu_title
+	 * @param $slug
+	 * @param $parent_slug
+	 * @param $capability
+	 * @param string $callback
+	 * @param null $position
+	 *
+	 * @return void
+	 */
+	static function add_sub_page(
+		$page_title,
+		$menu_title,
+		$slug,
+		$parent_slug,
+		$capability,
+		$callback = '',
+		$position = null
+	) {
+		self::$sub_pages[ $slug ] = [
+			"slug"        => $slug,
+			"parent_slug" => $parent_slug,
+			"capability"  => $capability,
+			"menu_title"  => $menu_title,
+			"page_title"  => $page_title,
+			"callback"    => $callback,
+			"position"    => $position,
 		];
 	}
 
 
+	/**
+	 * Adding menu items
+	 *
+	 * @return void
+	 */
 	static function add_menu_pages() {
 		foreach ( self::$pages as $page ) {
+			$slug = self::$slug_prefix ? self::$slug_prefix . '-' . $page['slug'] : $page['slug'];
 			add_menu_page(
+				$page['page_title'],
+				$page['menu_title'],
+				$page['capability'],
+				$slug,
+				$page['callback'],
+				$page['icon_url'],
+				$page['position']
+			);
+		}
+
+		// add submenu
+		foreach ( self::$sub_pages as $page ) {
+			add_submenu_page(
+				self::$slug_prefix ? self::$slug_prefix . '-' . $page['parent_slug'] : $page['parent_slug'],
 				$page['page_title'],
 				$page['menu_title'],
 				$page['capability'],
 				self::$slug_prefix ? self::$slug_prefix . '-' . $page['slug'] : $page['slug'],
 				$page['callback'],
-				$page['icon_url'],
 				$page['position']
 			);
+		}
+
+		// remove submenu
+		foreach ( self::$pages as $page ) {
+			$slug = self::$slug_prefix ? self::$slug_prefix . '-' . $page['slug'] : $page['slug'];
+
+			// remove main pages from submenu
+			if ( isset( $page['remove_from_submenu'] ) && $page['remove_from_submenu'] ) {
+				remove_submenu_page( $slug, $slug );
+			}
 		}
 	}
 
--- src/Admin/admin.menu.php
+++ src/Admin/admin.menu.php
@@ -5,10 +5,19 @@
 use AwwwesomeEEP\Admin\Controllers\Dashboard;
 use AwwwesomeEEP\Includes\Core\Admin_Menu;
 
+// ADD Main Awwwesome Menu Page
 Admin_Menu::add_admin_page(
+	'Awwwesome',
+	'Awwwesome',
+	'awwwesome',
+	'manage_options' // TODO: pořešit, nevím jaké lze použít
+);
+
+Admin_Menu::add_sub_page(
 	'Elementor Extension Pack',
 	'EEP',
 	'dashboard',
+	'awwwesome',
 	'manage_options', // TODO: pořešit, nevím jaké lze použít
 	[ new Dashboard(), 'index' ]
 );
